7b4bb640af3379336dae5fa5857fd71f94eab1fc
cheruden <cheruden25@gmail.com>
2021-07-19 13:31:32 +0300

    change the url for my registration form

--- awardproject/urls.py
+++ awardproject/urls.py
@@ -13,17 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import include,path
-from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    
-    path('accounts/register/',RegistrationView.as_view(success_url='/'),name='django_registration_register'),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include ('awardsapp.urls')),
-    
-
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('awardsapp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^api-token-auth/', obtain_auth_token),
 ]
